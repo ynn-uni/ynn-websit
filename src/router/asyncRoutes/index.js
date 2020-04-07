c8eb3c6f48335f6051c32a14cf9102a2f9e55976
cln1994 <635825582@qq.com>
2020-04-07 15:25:23 +0800

    update

--- src/router/asyncRoutes/index.js
+++ src/router/asyncRoutes/index.js
@@ -12,36 +12,57 @@ export default [
       {
         path: 'home',
         name: 'home',
+        meta: {
+          title: '首页'
+        },
         component: () => import('@/views/home/index')
       },
       {
         path: 'news',
         name: 'news',
+        meta: {
+          title: '新闻列表'
+        },
         component: () => import('@/views/news/index')
       },
       {
         path: 'newsdetail',
         name: 'newsdetail',
+        meta: {
+          title: '新闻详情'
+        },
         component: () => import('@/views/news/newsdetail/index')
       },
       {
         path: 'recruit',
         name: 'recruit',
+        meta: {
+          title: '招募列表'
+        },
         component: () => import('@/views/recruit/index')
       },
       {
         path: 'recruitdetail',
         name: 'recruitdetail',
+        meta: {
+          title: '招募详情'
+        },
         component: () => import('@/views/recruit/recruitdetail/index')
       },
       {
         path: 'teams',
         name: 'teams',
+        meta: {
+          title: '专家团队'
+        },
         component: () => import('@/views/teams/index')
       },
       {
         path: 'about',
         name: 'about',
+        meta: {
+          title: '关于我们'
+        },
         component: () => import('@/views/about/index')
       }
     ]
